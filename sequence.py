7fac233547357f9cf0431d3fc0aaf94d79ae0c5c
meugr <meugr32@gmail.com>
2019-02-26 22:15:06 +0300

    Update to ver1.1
    Added ID and description of sequences in the output file FASTA.
    Edited text messages in the console.

--- sequence.py
+++ sequence.py
@@ -1,4 +1,4 @@
-# ver 1.0
+# ver 1.1
 # Получение последовательности нуклеотидов между двумя заданными
 # последовательностями. Обрабатывает заданную, комплиментарную и
 # обратные им последовательности.
@@ -6,8 +6,8 @@
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
-from os import listdir, path
 from Bio.Data import IUPACData
+from os import listdir, path
 
 
 def get_sequence(path_file):
@@ -17,7 +17,7 @@ def get_sequence(path_file):
     """
     with open(path_file, "rb") as input_file:
         for i in SeqIO.parse(input_file, "abi"):
-            sequence = Seq("".join(i[:]))
+            sequence = Seq("".join(i[:]),)
         return sequence
 
 
@@ -57,12 +57,20 @@ def find_sequence(sequence, sequence_reverse,
                   start, end):
     sequences = (sequence, sequence_reverse, comp_sequence,
                  comp_sequence_reverse)
+    seq_type = -1
     for i in sequences:
+        seq_type += 1
+
         seq_start = search(str(i), start)
         seq_end = search(str(i), end)
         if seq_start and seq_end:
-            print(seq_start, seq_end)
-            return i[seq_start: seq_end + len(end)]
+            total_cou = 0
+            for c in ('A', 'C', 'T', 'G'):
+                cou = i[seq_start: seq_end + len(end)].count(c)
+                total_cou += cou
+            final_seq = i[seq_start: seq_end + len(end)]
+            return final_seq, seq_start, seq_end,\
+                seq_type, len(final_seq) - total_cou
 
 
 def write_in_output_file(output_sequence_list):
@@ -91,7 +99,7 @@ def write_in_error_file(output_error_list):
 print("""
 Получение последовательности нуклеотидов между двумя заданными
 последовательностями. Обрабатывает заданную, комплиментарную и
-обратные им последовательности.
+обратные им последовательности. Учитывает неоднозначные нуклеотиды.
 
 КАК ПОЛЬЗОВАТЬСЯ
 Создайте в директории с программой директорию 'input', куда положите
@@ -99,6 +107,11 @@ print("""
 Введите начальную и конечную последовательность
 Результат будет в файле output.fasta, список необработанных файлов
 в error.txt
+
+id последовательности - имя файла.
+description - длина вставки, положение в плазмиде,
+прямая, реверснутая, комплиментарная или комплиментарнвя реверснутая,
+количество неопределенностей
 """)
 
 subdir = 'input'
@@ -107,6 +120,8 @@ end = input('Введите конец последовательности: ').
 output_sequence_list = []
 output_error_list = []
 filename_list = listdir(subdir)
+seq_types = ('sequence', 'sequence_reverse', 'comp_sequence',
+             'comp_sequence_reverse')
 
 for filename in filename_list:
     path_file = path.join(subdir, filename)
@@ -116,18 +131,31 @@ for filename in filename_list:
     comp_sequence = make_complimentary(sequence)
     comp_sequence_reverse = comp_sequence[::-1]
 
-    sequence_find = find_sequence(sequence, sequence_reverse,
-                                  comp_sequence, comp_sequence_reverse,
-                                  start, end)
+    sequence_find_info = find_sequence(sequence, sequence_reverse,
+                                       comp_sequence, comp_sequence_reverse,
+                                       start, end)
 
-    if sequence_find:
+    if sequence_find_info:
+        # сбор данных для description
+        length = len(sequence_find_info[0])
+        ambiguity = sequence_find_info[4]
+        description = f'length: {length}, \
+position in the plasmid: {sequence_find_info[1:3]}, seq_type: \
+{seq_types[sequence_find_info[3]]}, ambiguity: {ambiguity}'
         # Записываем последовательность в список.
-        output_sequence_list.append(SeqRecord(sequence_find))
-        print(sequence_find)
+        output_sequence_list.append(SeqRecord(sequence_find_info[0],
+                                              id=filename,
+                                              description=description))
+        print(sequence_find_info[0])
     else:
         # Записываем название файла, в котором возникла ошибка.
         output_error_list.append(filename)
 
 write_in_output_file(output_sequence_list)
 write_in_error_file(output_error_list)
-print('Готово!', len(output_sequence_list))
+print(f'''
+=========================================================
+Готово!
+Обработано файлов: {len(filename_list)}
+Получено последовательностей: {len(output_sequence_list)}
+''')
